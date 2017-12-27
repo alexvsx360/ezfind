2df846ac9bc8fb252b67706af760b012570a1d25
Jacob <yaki.ams@gmail.com>
2017-12-27 12:37:19 +0200

    new sale form for ezfind

---
+++ ezfind/index.php
@@ -0,0 +1,180 @@
+<?php
+//ini_set('error_reporting', E_ALL);
+//in/i_set('display_errors', 1);
+//ini_set('display_startup_errors', 1);
+?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN""http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=windows-1251"/>
+
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="css/bootstrap.min.css" crossorigin="anonymous">
+    <!-- Optional theme -->
+    <link rel="stylesheet" href="css/bootstrap-theme.min.css" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/style.css">
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <!-- Latest compiled and minified JavaScript -->
+    <script src="js/bootstrap.min.js" crossorigin="anonymous"></script>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <title>הגשת מכירה חדשה לאיזיפינד</title>
+</head>
+<body>
+<?php
+
+include ('../generalUtilities/functions.php');
+include ('../generalUtilities/leadImFunctions.php');
+
+
+$customerFullName = ""; $customerPhone = ""; $ssn =""; $email =""; $callCenterName = ""; $recordNumber = "";
+$address = ""; $secondaryCustomerName = ""; $secondaryCustomerSsn = "";
+$userName = ""; $userEmail = ""; $issueDate = ""; $sellingChannel="";
+if ($_GET) {
+    global $customerFullName,
+           $customerPhone,
+           $ssn,
+           $email,
+           $secondaryCustomerSsn,
+           $secondaryCustomerName,
+           $address,
+           $userName,
+           $userEmail,
+           $sellingChannel,
+           $issueDate,
+           $callCenterName;
+
+    /*get lead information from the CRM*/
+    $acc_id = $_GET['crmAcccountNumber'];
+    $recordNumber = $_GET['recordNumber'];
+    $leadToPopulateJson = getLeadJson($_GET['recordNumber'], $acc_id, $_GET['agentId']);
+    $customerPhone = getCustomerPhone($acc_id, $leadToPopulateJson);
+    $customerFullName = getCustomerFullName($acc_id, $leadToPopulateJson);
+    $ssn = getCustomerSsn($acc_id, $leadToPopulateJson);
+    $email = getCustomerEmail($acc_id, $leadToPopulateJson);
+    $callCenterName = getCallCenterName($acc_id, $leadToPopulateJson);
+    $address = getCustomerAddress($acc_id, $leadToPopulateJson);
+    $address = str_replace("'", "", $address);
+    $userName = $leadToPopulateJson['user']['name'];
+    $userEmail = $leadToPopulateJson['user']['email'];
+
+    $issueDate = $leadToPopulateJson['lead']['fields']['94522'];
+    $sellingChannel = $leadToPopulateJson['lead']['channel_name'];
+    $secondaryCustomerName = getSecondaryCustomerName($acc_id, $leadToPopulateJson);
+    $secondaryCustomerSsn = getSecondaryCustomerSsn($acc_id, $leadToPopulateJson);
+
+}
+
+?>
+<div class="container" role="main" id="back_form">
+
+
+    <div class="text-center">
+        <img src="logo3.png" class="rounded">
+    </div>
+    <div class="row" >
+        <form id="main-form" action="newSale.php" class="" method="post" >
+            <div class="form-group">
+                <input type="hidden" class="input-group form-control" value="<?php print $customerFullName; ?>"  name="customerName"/>
+                <input type="hidden" class="input-group form-control" value="<?php print $customerPhone; ?>" name="customerPhone"/>
+                <input type="hidden" class="input-group form-control" value="<?php print $ssn ?>"  name="customerSsn"/>
+                <input type="hidden" class="input-group form-control" value="<?php print $email ?>"  name="customerEmail"/>
+                <input type="hidden" class="input-group form-control" value="<?php print $callCenterName ?>" name="callCenterName"/>
+                <input type="hidden" class="input-group form-control" value="<?php print $recordNumber ?>" name="recordNumber"/>
+                <input type="hidden" class="input-group form-control" value="<?php print $secondaryCustomerName ?>" name="secondaryCustomerName"/>
+                <input type="hidden" class="input-group form-control" value="<?php print $secondaryCustomerSsn ?>" name="secondaryCustomerSsn"/>
+                <input type="hidden" class="input-group form-control" value="<?php print $address ?>" name="address"/>
+                <input type="hidden" class="input-group form-control" value="<?php print $userName ?>" name="userName"/>
+                <input type="hidden" class="input-group form-control" value="<?php print $userEmail ?>" name="userEmail"/>
+                <input type="hidden" class="input-group form-control" value="<?php print $issueDate ?>" name="issueDate"/>
+                <input type="hidden" class="input-group form-control" value="<?php print $sellingChannel ?>" name="sellingChannel"/>
+            </div>
+            <div class="row" >
+                <div class="col-xs-4"></div>
+                <div class="col-xs-10 col-sm-4 col-md-4 col-lg-4">
+                    <div class="input-group">
+                        <div id="radioBtn" class="btn-group">
+                            <label for="sel1">תיק יחיד או זוגי?</label><br>
+                            <a class="btn btn-primary btn-sm notActive" data-toggle="happy" data-title="2">זוגי</a>
+                            <a class="btn btn-primary btn-sm active" data-toggle="happy" data-title="1">יחיד</a>
+                        </div>
+                        <input type="hidden" name="customerCount" id="customerCount" value="1">
+                    </div>
+                </div>
+            </div>
+            <div class="row" >
+                <div class="col-xs-4 "></div>
+                <div class="col-xs-10 col-sm-4 col-md-4 col-lg-4">
+                    <label for="sel1">סכום העסקה</label>
+                    <input required type="number" class="input-group form-control" placeholder="סכום העסקה" name="paymentSum"/>
+                </div>
+            </div>
+            <div class="row" >
+                <div class="col-xs-4 "></div>
+                <div class="col-xs-10 col-sm-4 col-md-4 col-lg-4">
+                    <label for="sel1">מספר תשלומים</label>
+                    <input required type="number" class="input-group form-control" placeholder="משפר תשלומים" name="paymentCount"/>
+                </div>
+            </div>
+            <div class="row" >
+                <div class="col-xs-4 "></div>
+                <div class="col-xs-10 col-sm-4 col-md-4 col-lg-4">
+                    <input type="submit" class="btn btn-primary" id="submit" name="sendForm" value="הגש מכירה"/>
+                </div>
+            </div>
+        </form>
+    </div>
+
+
+
+    <script>
+
+        jQuery(document).ready(function () {
+
+            $('#radioBtn a').on('click', function(){
+                var sel = $(this).data('title');
+                var tog = $(this).data('toggle');
+                $('#'+tog).prop('value', sel);
+                $('#customerCount').val(sel);
+
+
+                $('a[data-toggle="'+tog+'"]').not('[data-title="'+sel+'"]').removeClass('active').addClass('notActive');
+                $('a[data-toggle="'+tog+'"][data-title="'+sel+'"]').removeClass('notActive').addClass('active');
+            });
+
+
+
+
+            jQuery("#back_to_form").click(function () {
+                jQuery('#back_form').removeClass('hide');
+                jQuery('#button_block').hide();
+            });
+
+
+            jQuery('select#category').on('change', function (e) {
+                var optionSelected = jQuery("option:selected", this);
+                var valueSelected = this.value;
+
+                jQuery('.cat_name').val(optionSelected.text());
+            });
+            jQuery("#main-form").submit(function () {
+                $(this).find(':submit').val("פותח פניה...").attr('disabled', 'disabled');
+            });
+
+            jQuery("#submit").click(function () {
+                var ddd = jQuery("#InputFile").val();
+                if (ddd == "") {
+                    jQuery('.error').show();
+                    return false;
+                }
+                else {
+                    jQuery('.error').hide();
+                    return true;
+                }
+            });
+        });
+    </script>
+
+</body>
+</html>
---
+++ ezfind/newSale.php
@@ -0,0 +1,152 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=windows-1251"/>
+
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <!-- Optional theme -->
+    <link rel="stylesheet" h    ref="css/bootstrap-theme.min.css">
+    <link rel="stylesheet" href="css/style.css">
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <!-- Latest compiled and minified JavaScript -->
+    <script src="js/bootstrap.min.js" crossorigin="anonymous"></script>
+
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <title>הפניה נתפחה בהצלחה!</title>
+</head>
+<body>
+
+<?php
+/**
+ * Created by PhpStorm.
+ * User: User
+ * Date: 12/20/2017
+ * Time: 3:32 PM
+ */
+include ('../generalUtilities/functions.php');
+include ('../generalUtilities/leadImFunctions.php');
+$data = "";
+
+function generateMislakeLead($ticketCreationResponse){
+    return [
+        'lm_form' => 18681,
+        'lm_key' => "7fa3e74b41",
+        'lm_redirect' => "no",
+        'name' => $_POST['customerName'],
+        'phone' => $_POST['customerPhone'],
+        'id' => $_POST['customerSsn'],
+        'issueDate' => $_POST['issueDate'],
+        'email' => $_POST['customerEmail'],
+        'address' => $_POST['address'],
+        'callCenter' => $_POST['callCenterName'],
+        'ticketNumber' => $ticketCreationResponse['ticket']['id'],
+        'zendeskLink' => 'https://ezfind-sherut.zendesk.com/agent/tickets/' .$ticketCreationResponse['ticket']['id'],
+        'sellingChannel' => $_POST['sellingChannel'],
+        'sellerName' => $_POST['userName'],
+        'paymentSum' => $_POST['paymentSum'],
+        'mislakaPaymentCount' => $_POST['paymentCount'],
+        'customerCount' => $_POST['customerCount'],
+        'customerType' => ($_POST['customerCount'] == 2 ? 'זוגי' : 'יחיד' ),
+    ];
+}
+
+function generateCustomerPostData(){
+    return [
+        'lm_form' => '17993',
+        'lm_key' => '6ebab5cef5',
+        'lm_redirect' => 'no',
+        /*'lm_supplier' => $agentId,*/
+        'name' => $_POST['customerName'],
+        'phone' => $_POST['customerPhone'],
+        'id' => $_POST['customerSsn'],
+        'issueDate' => $_POST['issueDate'],
+        'email' => $_POST['customerEmail'],
+        'address' => $_POST['address'],
+        'callCenter' => $_POST['callCenterName']
+    ];
+}
+
+function generateTicketComment(){
+    global $data;
+    if ($_POST['customerCount'] == 2){
+        $data->comment = array(
+            'body' =>   'מס\' ליד במע\':	 ' .$_POST['recordNumber'] . " \n" .
+                'קישור ישיר לליד\':' . "https://crm.ibell.co.il/a/3328/leads/". $_POST['recordNumber']  ." \n" .
+                'שם מלא:	' . $_POST['customerName'] . " \n" .
+                'מס\' ת.ז.:' . $_POST['customerSsn'] . "\n" .
+                'כתובת:' . $_POST['address'] . " \n" .
+                'פרטי בן/בת זוג' .  " \n" .
+                'שם מלא:' . $_POST['secondaryCustomerName'] . " \n" .
+                'מס\' ת.ז.:' . $_POST['secondaryCustomerSsn'] . "\n" .
+                'כתובת:' . $_POST['address'] . " \n"
+        );
+    }else{
+        $data->comment = array(
+            'body' =>   'מס\' ליד במע\':	 ' .$_POST['recordNumber'] . " \n" .
+                'קישור ישיר לליד\':' . "https://crm.ibell.co.il/a/3328/leads/". $_POST['recordNumber']  ." \n" .
+                'שם מלא:	' . $_POST['customerName'] . " \n" .
+                'מס\' ת.ז.:' . $_POST['customerSsn'] . "\n" .
+                'כתובת:' . $_POST['address'] . " \n"
+        );
+    }
+}
+
+var_dump($_REQUEST);
+
+/*open new Ticket*/
+$ticketUrl = "https://ezfind-sherut.zendesk.com/api/v2/tickets.json";
+$data->subject = "בדיקה במסלקה פנסיונית (" . $_POST['recordNumber'] . ")";
+
+$data->custom_fields = array('114102615553' => 'מסלקות_פנסיוניות___בקשות_חדשות');
+$data->requester = array(
+    'email' => 'a3328c17397@emails.lead.im',
+    'name' => 'LeadIm Proxy'
+);
+/*
+$data->collaborators = getCollaboratorArrayById($crmAccount, $userEmail); */
+generateTicketComment();
+$data->collaborators = array( $_POST['userEmail'], "yaki@ezfind.co.il", "shay@ezfind.co.il");
+
+/*API to Zendesk to open the ticket*/
+$create = json_encode(array('ticket' => $data)/*, JSON_UNESCAPED_UNICODE*/);
+//fwrite($myfile, "return value from Zendesk API is " . print_r($create, true));
+
+$username = 'yaki@tgeg.co.il/token';
+$password = 'r0sQ2m9H37u6OOnmYagEM08cW11xKasCbNZspYaF';
+
+$return = httpPostWithUserPassword($ticketUrl, $create, $username, $password);
+$ticketCreationResponse = json_decode($return, true);
+
+
+
+//create the mislaka
+$mislakaPost = generateMislakeLead($ticketCreationResponse);
+$newMislakaLeadId = openNewLead($mislakaPost);
+
+$updateCustomerResponse = addOrCreateCustomerandUpdateNewSale($newMislakaLeadId, $_POST['customerPhone'], generateCustomerPostData());
+echo $updateCustomerResponse;
+
+echo 1111;
+
+
+?>
+<div class="container" role="main" id="button_block">
+    <div class="text-center">
+        <img src="logo3.png" class="rounded">
+    </div>
+    <div class="row">
+        <div class="col-xs-12 col-md-12 col-sm-12">
+            <fieldset>
+                <div class="col-xs-6 col-md-6 col-sm-6">
+                    <a href="#" class="btn btn-success">הבקשה נשלחה כמו שצריך!</a>
+                </div>
+            </fieldset>
+        </div>
+    </div>
+</div>
+
+    </body>
+</html>
