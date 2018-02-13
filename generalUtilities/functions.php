f80111e094e532347d6a4ab5afb1193651a1672e
Jacob <yaki.ams@gmail.com>
2018-02-13 19:10:08 +0200

    bug fix

--- generalUtilities/functions.php
+++ generalUtilities/functions.php
@@ -81,7 +81,7 @@ function httpPostContentTypeJson($url, $post) {
     curl_setopt($ch, CURLOPT_URL, $url);
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
     curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));
-    curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($post));
+    curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
     $response = curl_exec($ch);
     curl_close($ch);
 
