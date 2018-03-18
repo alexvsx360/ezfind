d271a62fab8a311aa421ed6b78a8acbf096cd915
Jacob <yaki.ams@gmail.com>
2018-03-18 18:55:41 +0200

    update new 2 felds in The CRM תאריך שליחה לחברת הביטוח תאריך השלמת חוסר

--- functions.php
+++ functions.php
@@ -4,7 +4,48 @@
  * User: User
  * Date: 11/15/2017
  * Time: 8:10 AM
+
  */
+
+function getLead($lead_id, $acc_id, $lm_initializer_id)
+{
+    $leadImKey = '3765d732472d44469e70a088caef3040';
+
+    $populateLeadPostData = [
+        'key' => $leadImKey,
+        'acc_id' => $acc_id,
+        'lead_id' => $lead_id
+    ];
+    if (isset($lm_initializer_id)){
+        $populateLeadPostData['lm_initializer_id'] = $lm_initializer_id;
+
+    }
+
+    $leadInfoStr = httpPost('http://proxy.leadim.xyz/apiproxy/acc3305/getlead.ashx', $populateLeadPostData);
+    $leadToPopulateJson = json_decode($leadInfoStr, true);
+    return $leadToPopulateJson;
+}
+
+
+
+function getPolicyLeadByTicketID($acc_id, $leadTicket){
+    $leadImKey = '3765d732472d44469e70a088caef3040';
+
+    $populateLeadPostData = [
+        'key' => $leadImKey,
+        'acc_id' => $acc_id,
+        'searchby' => "102158",         //ticketNumber
+        "campaign"  => "17967",         //Policy campaign
+        'searchterm' => $leadTicket
+    ];
+
+    $leadInfoStr = httpPost('http://proxy.leadim.xyz/apiproxy/acc3305/searchlead.ashx' , $populateLeadPostData);
+    $leadToPopulateJson = json_decode($leadInfoStr, true);
+    if ($leadToPopulateJson['lead_id'] > 0){
+        return getLead($leadToPopulateJson['lead_id'], $acc_id, null);
+    }
+    return null;
+}
 
 function httpGet($url){
     $ch = curl_init();
@@ -94,7 +135,7 @@ function getCollaboratorArrayById($crmAccountNum, $userEmail) {
     }
 }
 
-function updateBIScreen($agentId, $insuranceCompany, $premia, $hitum, $sellerName, $promoterName, $leadChannel, $productionStatus, $insuranceType, $customerId, $moked){
+function createRecordInBIScreen($saleDate, $agentId, $insuranceCompany, $premia, $hitum, $sellerName, $promoterName, $leadChannel, $productionStatus, $insuranceType, $recordId, $moked){
     $registration = array(
         'data_source' => 'ed3d7226947140b996ecf1bfbd19577e',    // Insurance Sales UUI
         //'member' => $agentId,                                   //either provide the member uuid or member_api_provider and member_api_id and member_name
@@ -102,36 +143,88 @@ function updateBIScreen($agentId, $insuranceCompany, $premia, $hitum, $sellerNam
         'member_api_id' => $agentId,  # Provide a user ID from the above system.
         'member_name' => $sellerName,  # Provide the user ID from the above system
         #'team': '<Team UUID>', #optional make registration for team
-        // 'date' => '2012-13-13T00:00:00', // Optional, if not provided date is now (ISO-8601 format)
-        'Insurance Type' => $insuranceType,
-        'Insurance Company' => $insuranceCompany,
+        'date' => date('Y-m-d\TH:i:s', $saleDate), // Optional, if not provided date is now (ISO-8601 format)
+        'Insurance_Type' => $insuranceType,
+        'Insurance_Company' => $insuranceCompany,
+        'Monthly_Premia' => $premia,
+        'Annual_Premia' => ($premia * 12),
+        'hitum' => $hitum,
+        'Seller_Name' => $sellerName,
+        'Promoter_Name' => $promoterName,
+        'Selling_Channel' => $leadChannel,
+        'Production_Status' => $productionStatus,
+        'Call_Center_name' => $moked,
+        'Sale_Date' => date('Y-m-d\TH:i:s', $saleDate),
+        'Production_Date' => date('Y-m-d\TH:i:s', $saleDate),
+        'reference' => $recordId  // Change to a unique ID
+    );
+    updatePlecto($registration);
+}
+
+function updateRecordInBiScreen($recordId, $productionStatus, $productionDateAsTimestamp, $premia){
+
+    $searchBaseUrl = "http://proxy.leadim.xyz/apiproxy/acc3305/searchlead.ashx";
+    $searchPost = [
+        "key" => "3765d732472d44469e70a088caef3040",
+        "acc_id" => "3694",
+        "searchby" => "102158",              // Ticket Number
+        "searchterm" => $recordId,
+        "campaign"  => "17967"            // Policy campaign
+        //"channel"   => "17993"
+    ];
+
+
+    $leadInfoStr = httpPost($searchBaseUrl , $searchPost);
+    $leadToPopulateJson = json_decode($leadInfoStr, true);
+    $fieldsValuesJsonArray = array_values($leadToPopulateJson['lead']['fields']);
+    $fullName = $fieldsValuesJsonArray[0] . " " . $fieldsValuesJsonArray[1];
+    $phone =  $fieldsValuesJsonArray[3];
+    $promoterName = $fieldsValuesJsonArray[16];
+    $currentUserName = $leadToPopulateJson['user']['name'];
+    $currentUserEmail = $leadToPopulateJson['user']['email'];
+    $channelName = $leadToPopulateJson['lead']['channel_name'];
+
+
+
+
+
+    $registration = array(
+        'data_source' => 'ed3d7226947140b996ecf1bfbd19577e',    // Insurance Sales UUI
+        //'member' => $agentId,                                   //either provide the member uuid or member_api_provider and member_api_id and member_name
+        'member_api_provider' => 'lead im CRM',  # Use appropriate name for the system that use the below ID. Can also be e.g. salesforce, podio, pipedrive or the name of any external system.
+        #'team': '<Team UUID>', #optional make registration for team
         'Monthry Premia' => $premia,
         'Annual Premia' => ($premia * 12),
-        'hitum' => $hitum,
-        'Seller Name' => $sellerName,
-        'Promoter Name' => $promoterName,
-        'Selling Channel' => $leadChannel,
         'Production Status ' => $productionStatus,
-        'Call Center name' => $moked,
-        'reference' => $customerId  // Change to a unique ID
+        'Production Date' => date('c', $productionDateAsTimestamp),
+        'reference' => $recordId  // Change to a unique ID
     );
-    $json_registration = json_encode ( $registration );
+    updatePlecto($registration);
+}
 
-    $ch = curl_init( "https://app.plecto.com/api/v2/registrations/" );
-    curl_setopt( $ch, CURLOPT_RETURNTRANSFER, 1 );
-    curl_setopt( $ch, CURLOPT_HTTPAUTH, CURLAUTH_BASIC );
-    curl_setopt( $ch, CURLOPT_USERPWD, "yaki@tgeg.co.il:" . "alma@102030" );
+/**
+ * @param $registration
+ */
+function updatePlecto($registration): void
+{
+    $json_registration = json_encode($registration);
 
-    curl_setopt( $ch, CURLOPT_HTTPHEADER, array ( "Content-Type: application/json; charset=utf-8" ) );
-    curl_setopt( $ch, CURLOPT_POST, 1 );
-    curl_setopt( $ch, CURLOPT_POSTFIELDS, $json_registration );
+    //$ch = curl_init("https://app.plecto.com/api/v2/registrations/");
+    $ch = curl_init("http://proxy.leadim.xyz/logger.ashx");
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    curl_setopt($ch, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
+    curl_setopt($ch, CURLOPT_USERPWD, "yaki@tgeg.co.il:" . "alma@102030");
 
-    $output = curl_exec( $ch );
+    curl_setopt($ch, CURLOPT_HTTPHEADER, array("Content-Type: application/json; charset=utf-8"));
+    curl_setopt($ch, CURLOPT_POST, 1);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, $json_registration);
 
-    $errno = curl_errno( $ch );
+    $output = curl_exec($ch);
+
+    $errno = curl_errno($ch);
     // Handle errors
 
-    $output = json_decode( $output, true );
+    $output = json_decode($output, true);
 
     curl_close($ch);
 }
--- updatecrm.php
+++ updatecrm.php
@@ -78,6 +78,23 @@ function appendParameterToURL ($fieldToUpdate, $fieldValue){
             appendParameterToURL(102141, ($_GET['cancelationLetterSent'] == 0 ? "lfv102143" : "lfv102142") );
             }*/
 
+            /*Check if the ticket was transfered to תור_הפקות*/
+            if (!empty($_GET['status']) && $_GET['status'] == "תור_הפקות"){
+                //get lead status from lead im CRM
+                $leadToPopulateJson = getPolicyLeadByTicketID(3694, $_GET['lead_ticket']);
+                if ($leadToPopulateJson != null){
+                    echo $leadToPopulateJson;
+                    if ($leadToPopulateJson['lead']['status'] == 102338  || $leadToPopulateJson['lead']['status'] == 102337 ){
+                        //מעבר מתור חיתום או תור חוסרים לתור הפקות
+                        //need to update "תאריך השלמת חוסר" - 106545
+                        appendParameterToURL(106545, time()); //current time milliseconds
+                    } else if($leadToPopulateJson['lead']['status'] == 100084){
+                        //מעבר מתור בקרה לחברת הביטוח
+                        //need to update field 106546 תאריך שליחה לחברת הביטוח
+                        appendParameterToURL(106546, time()); //current time milliseconds
+                    }
+                }
+            }
 
         fwrite($myfile, "Update URL is: " . $updateLeadUrl ."\n");
         /*Update the CRM*/
@@ -86,3 +103,4 @@ function appendParameterToURL ($fieldToUpdate, $fieldValue){
         //updateRecordInBiScreen($_GET['lead_ticket'],  $statusToFieldNumJson[$_GET['status']], strtotime($_GET['productionDate']), $_GET['premia']);
         /*http://proxy.leadim.xyz/apiproxy/acc3305/updatelead.ashx?acc_id=3694&lead_ticket=4564&status=10084&update_fields[fld_id]=102145&update_fields[fld_val]=&update_fields[fld_id_1]=100100&update_fields[fld_val_1]=123456&update_fields[fld_id_2]=102416&update_fields[fld_val_2]=154&update_fields[fld_id_3]=102140&update_fields[fld_val_3]=2017-12-01&update_fields[fld_id_4]=102218&update_fields[fld_val_4]=2017-11-27&update_fields[fld_id_5]=102144&update_fields[fld_val_5]=2018-01-01&update_fields[fld_id_6]=102133&update_fields[fld_val_6]=ירוק*/
     }
+
