af174c73d3096bd92433c8c39a7e555c851aa8b5
dinarozenberg <dina.r@ezfind.co.il>
2018-08-13 10:29:30 +0300

    Add a check if the ticket changes have been updated the crm  and if not create a error ticket in ezfind account of zendesk

--- ezfind/updateCrm.php
+++ ezfind/updateCrm.php
@@ -7,11 +7,69 @@
  */
 
 include_once ("../generalUtilities/functions.php");
+require '../vendor/autoload.php';
+use Zendesk\API\HttpClient as ZendeskAPI;
+
+$subdomain = "ezfind-sherut";
+$username  = "yaki@tgeg.co.il";
+$token     = "r0sQ2m9H37u6OOnmYagEM08cW11xKasCbNZspYaF"; // replace this with your token
+$client = new ZendeskAPI($subdomain, $username);
+$client->setAuth('basic', ['username' => $username, 'token' => $token]);
 var_dump($_REQUEST);
 $req_dump = print_r($_REQUEST, TRUE);
 $fp = fopen('request.log', 'a');
 fwrite($fp, $req_dump ."\n");
 ;
+function checkUpdate($result,$updateLeadUrl)
+{
+    global $client;
+    if ($result != "OK") {
+        for ($i = 0; $i < 3; $i++) {
+            sleep(1);
+            $result = httpGet($updateLeadUrl);
+            if ($result == "OK") {
+                break;
+            } else {
+                if ($i == 2 && $result != "OK") {
+                    $result = json_decode($result, true);
+                    $current_file_name = basename($_SERVER['PHP_SELF']);
+                    $debug = debug_backtrace();
+                    $debug = array_shift($debug);
+//                  $sentfromServer = $_SERVER['SERVER_NAME'];
+                    $sentfromServer = $_SERVER['HTTP_HOST'];
+                    $parseUrl = parse_url($updateLeadUrl);
+                    $sentToServer = $parseUrl['host'];
+                    $prameters ="";
+                    foreach($_GET as $key => $value){
+                        $prameters.= $key . " : " . $value . "\n";
+                    }
+                    $newTicket = $client->tickets()->create([
+                        'tags' => 'error',
+                        'status' => 'new',
+                        'subject' => 'error in update crm',
+                        'requester' => array(
+                            'name' => 'dina',
+                            'email' => 'dina.r@ezfind.co.il'
+                        ),
+                        //   'collaborators' =>  ["Yaki@tgeg.co.il"],
+                        'comment' => [
+                            'body' => 'in date:' . date("d/m/Y") . " \n" .
+                                'in file: ' . $current_file_name . " \n" .
+                                'in line: ' . $debug["line"] . " \n" .
+                                'prameters: ' . $prameters . " \n" .
+                                'Error:' . $result['errmsg'] . " \n" .
+                                'request sent from server :' . $sentfromServer . " \n" .
+                                'to server:' . $sentToServer . " \n" .
+                                'orignalUrl:' . $updateLeadUrl
+                        ]
+                    ]);
+                }
+            }
+        }
+    } else {
+        return $result;
+    }
+}
 $statusToFieldNumJson = [
     "מסלקות_פנסיוניות___בקשות_חדשות" => "100084",
     "מסלקות_פנסיוניות___נשלח_לחברת_הביטוח" => "100085",
@@ -38,6 +96,8 @@ $updateLeadUrl .= "&status=" . $statusToFieldNumJson[$_GET['workingQueue']];
 $updateLeadUrl .= "&update_fields[fld_id]=104484&update_fields[fld_val]=" . $_GET['workingQueue'];
 fwrite($fp, $updateLeadUrl."\n" );
 $response = httpGet($updateLeadUrl);
+checkUpdate($response,$updateLeadUrl);
 fwrite($fp, $response."\n" );
+
 fclose($fp);
 
